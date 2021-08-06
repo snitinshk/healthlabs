e15f4d8c409525b349ec241c5991d4d6faaf1544
Shivam Shukla <shshukla@oshshuklanew.local>
2021-08-06 13:09:48 +0530

    final design upates

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+/*/////   HTML5    //////*/
+document.createElement("header");  
+document.createElement("footer");  
+document.createElement("nav");  
+document.createElement("menu");  
+document.createElement("article");  
+document.createElement("section");
+document.createElement("aside");
+document.createElement("video");
+/*/////   HTML5 End   //////*/
+
+$(function(){ 
+
+
+ 
+ 
+ 
+});
+
+
+
+
+
